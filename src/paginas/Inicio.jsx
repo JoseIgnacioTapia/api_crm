1bcbb33db051a8de89ebffc5f8d0feb00361a4b1
JoseIgnacioTapia <josignaciot@gmail.com>
2022-03-13 20:08:09 -0300

    Consult the API to obtain the Clients

--- src/paginas/Inicio.jsx
+++ src/paginas/Inicio.jsx
@@ -1,6 +1,24 @@
-import React from 'react';
+import { useState, useEffect } from 'react';
 
 const Inicio = () => {
+  const [clientes, setclientes] = useState([]);
+
+  useEffect(() => {
+    const obtenerClientesAPI = async () => {
+      try {
+        const url = 'http://localhost:4000/clientes';
+        const respuesta = await fetch(url);
+        const resultado = await respuesta.json();
+
+        setclientes(resultado);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    obtenerClientesAPI();
+  }, []);
+
   return <div>Inicio.jsx</div>;
 };
 
