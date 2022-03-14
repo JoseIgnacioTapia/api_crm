669e49c771f6378f22e50d1210e237d49b9dbf87
JoseIgnacioTapia <josignaciot@gmail.com>
2022-03-14 20:48:02 -0300

    Updating API Records

--- src/components/Alerta.jsx
+++ src/components/Alerta.jsx
@@ -1,6 +1,6 @@
 const Alerta = ({ children }) => {
   return (
-    <div className="text-center my-4 bg-red-600 text-white font-bold uppercase">
+    <div className="text-center my-4 bg-red-600 text-white font-bold uppercase p-4">
       {children}
     </div>
   );
